4ffaa7214fd4ad1a36baa813ff215ef01c35bfd9
Dustin Wright <nzeroxpd@gmail.com>
2019-06-10 13:14:26 -0500

    Added getters & setters in Kb.java.

--- src/main/java/hello/KB.java
+++ src/main/java/hello/KB.java
@@ -1,6 +1,48 @@
 package hello;
 
 public class Kb {
+	
+	private String title;
+	private String keywords;
+	private String summary;
+	private String solution;
+	private String appliesTo;
+	
+	
+	public String getTitle() {
+		return title;  // should this be return this.title?
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getKeywords() {
+		return keywords;
+	}
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	public String getSolution() {
+		return solution;
+	}
+	public void setSolution(String solution) {
+		this.solution = solution;
+	}
+	public String getAppliesTo() {
+		return appliesTo;
+	}
+	public void setAppliesTo(String appliesTo) {
+		this.appliesTo = appliesTo;
+	}
+	
+	
+	
+	
 
 /*
 like personForm
